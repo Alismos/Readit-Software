2c60d0d76b1709bdd4dfda85e918faea3253a6eb
SantiagoSR <santiago.santa.ramirez@gmail.com>
2020-04-30 20:24:06 -0500

    updating

--- Readit/src/app/app.module.ts
+++ Readit/src/app/app.module.ts
@@ -9,10 +9,14 @@ import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { ListaComponent } from './components/lista/lista.component';
+import { ListaAddComponent } from './components/lista-add/lista-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    ListaAddComponent
   ],
   imports: [
     BrowserModule,
