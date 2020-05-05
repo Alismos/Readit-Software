1f0e7a4158e34b1dda7c68b363e78ae550c4c777
Alismos <daramirezs@eafit.edu.co>
2020-05-05 18:02:11 -0500

    Paquetes componentes assets incio

--- Readit/src/app/app.module.ts
+++ Readit/src/app/app.module.ts
@@ -4,26 +4,27 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//Importando servicios de angular
+/*Importando servicios de angular*/
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-//Importando servicio
+/*Importando servicio*/
 import { ConexionService } from './services/conexion.service';
 import { ListaComponent } from './components/lista/lista.component';
 import { ListaAddComponent } from './components/lista-add/lista-add.component';
 
 
-//Rutas
+/*Rutas*/
 
 import { app_routing } from './app.routes';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { RestaurantesComponent } from './components/restaurantes/restaurantes.component';
 import { PedidoComponent } from './components/pedido/pedido.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { PedidoComponent } from './components/pedido/pedido.component';
     InicioComponent,
     ContactoComponent,
     RestaurantesComponent,
-    PedidoComponent
+    PedidoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
